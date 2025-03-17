7d0344e2b476e5a989c8ec88686dde217fc41916
BoxyPipesnake <felipe_motoss@hotmail.com>
2025-03-16 20:31:04 -0600

    Implementa funcion actualizarListaAmigos

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-const listaAmigos = [];
+const amigos = [];
+
+const lista = document.getElementById('listaAmigos'); // Lista Amigos que se muestra en la pantalla
+
 
 function agregarAmigo(){
     const inputUsuario = document.getElementById('amigo');
@@ -8,8 +11,20 @@ function agregarAmigo(){
     if(inputUsuario.value === ''){
         alert('Por favor, inserte un nombre.');
     } else {
-        listaAmigos.push(inputUsuario.value);
+        amigos.push(inputUsuario.value);
         inputUsuario.value = '';
+        actualizarListaAmigos();
+        console.log(amigos);
     }
+}
+
+function actualizarListaAmigos(){
+    lista.textContent = '';
+    for(let i = 0; i < amigos.length; i++){
+        const li = document.createElement('li');
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
+
 }
 

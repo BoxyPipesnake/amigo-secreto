7d461fc48ee1ee469e5f0f7dacb8d66a01953a47
BoxyPipesnake <felipe_motoss@hotmail.com>
2025-03-16 20:13:42 -0600

    Implementa funcion agregarAmigos

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-const nombreAmigos = [];
+const listaAmigos = [];
+
+function agregarAmigo(){
+    const inputUsuario = document.getElementById('amigo');
+
+    if(inputUsuario.value === ''){
+        alert('Por favor, inserte un nombre.');
+    } else {
+        listaAmigos.push(inputUsuario.value);
+        inputUsuario.value = '';
+    }
+}
+
